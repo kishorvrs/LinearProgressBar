75e61ff0737a6a27bb756133a7c185af30031bc5
kishorvrs <kishorvrs@gmail.com>
2020-11-24 09:42:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/linearprogressbar/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.linearprogressbar;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    Handler handler;
+    Runnable runnable;
+    Timer timer;
+    int i=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar=(ProgressBar)findViewById(R.id.progressbar_id);
+        progressBar.setVisibility(View.VISIBLE);
+        progressBar.setProgress(0);
+        progressBar.setSecondaryProgress(0);
+        progressBar.setMax(100);
+        handler= new Handler();
+        runnable=new Runnable() {
+            @Override
+            public void run() {
+               if (++i<=100){
+                   progressBar.setProgress(i);
+                   progressBar.setSecondaryProgress(i+10);
+
+               }else{
+                     timer.cancel();
+               }
+            }
+        };
+        timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(runnable);
+            }
+        },8000,1000);
+
+    }
+}
